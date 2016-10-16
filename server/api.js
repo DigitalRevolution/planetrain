b1be2a1e0728dd9f7154d65e2af3b1b6ccf70e21
Digital Revolution <stephen.vandelinder@gmail.com>
2016-10-16 12:46:08 -0600

    testing db

--- server/api.js
+++ server/api.js
@@ -6,7 +6,7 @@ var db;
 
 if(NODE_ENV === 'production'){
     db = mongoose.connect(MONGO_URI);
-    console.log('connected to production database')
+    console.log('connected to production database');
 } else {
     db = mongoose.connect('mongodb://localhost/gtfsmin');
     console.log('connected to local database')
@@ -25,5 +25,5 @@ if(NODE_ENV === 'production'){
 //var StopTime = db.model('StopTime');
 //var Trip = db.model('Trip');
 
-//mongodb://gtfsapplication:EVL#2d=E!r;h7RFXd:H-R`@ds017070.mlab.com:17070/gtfs
+
 
